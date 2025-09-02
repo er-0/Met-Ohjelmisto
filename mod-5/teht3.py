c15f1e31eadbdc34f45a3d31f32be7be9fb96c58
er-0 <95697943+er-0@users.noreply.github.com>
2025-09-02 16:24:00 +0300

    Moduuli 6 tehty

--- mod-5/teht3.py
+++ mod-5/teht3.py
@@ -1,5 +1,3 @@
-'''Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun ja ilmoittaa, onko se alkuluku.
-Tässä tehtävässä alkulukuja ovat luvut, jotka ovat jaollisia vain ykkösellä ja itsellään.'''
 number = int(input("Anna kokonaisluku: "))
 prime = True
 if number < 2:
